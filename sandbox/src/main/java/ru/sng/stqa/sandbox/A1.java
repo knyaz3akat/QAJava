1045b2d4b5c68579199327f0ae24bb8772a9211c
knyaz3akat <98853943+knyaz3akat@users.noreply.github.com>
2022-02-14 00:30:07 +0300

    Проект отвязан в среде разработке от ГИТ

--- sandbox/src/main/java/ru/sng/stqa/sandbox/A1.java
+++ sandbox/src/main/java/ru/sng/stqa/sandbox/A1.java
@@ -1,7 +1,7 @@
 package ru.sng.stqa.sandbox;
 
 public class A1 {
-	public static void main(String[] args){
-		System.out.println("Hellow");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+    }
 }
